c7d78c3a7ad71e453fe06caea9c6634878c97adb
Elson Ramos <elson.0509@gmail.com>
2021-01-18 12:17:13 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <SafeAreaView>
+      <Text>Calculadora de IMC</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
